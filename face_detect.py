aac3d62adf80836176d7d9ade87f24d5d367c731
Amal <amalv@qburst.com>
2016-09-20 11:38:23 +0530

    Code refactoring

--- face_detect.py
+++ face_detect.py
@@ -15,9 +15,8 @@ SUCCESS_SOUND = DATA_PATH + 'hello.wav'
 INTERVAL_IN_SECS = 0.2
 IMAGES = []
 FACE_COUNT = 1
-
-recognizer = cv2.createLBPHFaceRecognizer()
-path="images/"
+RECOGNIZER = cv2.createLBPHFaceRecognizer()
+IMAGE_FOLDER_PATH="images/"
 
 def start_webcam(mirror=False):
     """Start capture on webcam"""
@@ -61,7 +60,7 @@ def detect_faces(image):
     for (x, y, w, h) in faces:
         try:
             print "Performing Image Prediction"
-            img , score = recognizer.predict(cv2.cvtColor(image[y: y + h, x: x + w], cv2.COLOR_BGR2GRAY))
+            img , score = RECOGNIZER.predict(cv2.cvtColor(image[y: y + h, x: x + w], cv2.COLOR_BGR2GRAY))
             if score > 200:
                 print "Match ignoring due to huge difference",score
                 sys.exit(1)
@@ -73,9 +72,8 @@ def detect_faces(image):
             print "Training"
             cv2.imwrite("images/" + str(FACE_COUNT) + ".jpeg", image[y: y + h, x: x + w])
             FACE_COUNT = FACE_COUNT + 1
-            image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+            image_paths = [os.path.join(IMAGE_FOLDER_PATH, f) for f in os.listdir(IMAGE_FOLDER_PATH)]
             for image_path in image_paths:
-                print image_path
                 image_pil = Image.open(image_path).convert('L')
                 image_set = np.array(image_pil, 'uint8')
                 nbr = os.path.split(image_path)[1].split(".")[0]
@@ -83,7 +81,7 @@ def detect_faces(image):
                 images.append(image_set)
                 labels.append(nbr)
             print np.array(labels)
-            recognizer.train(images, np.array(labels))
+            RECOGNIZER.train(images, np.array(labels))
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = image[y:y+h, x:x+w]
@@ -96,7 +94,7 @@ def trainwith_existing_images():
     global FACE_COUNT
     images=[]
     labels=[]
-    image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+    image_paths = [os.path.join(IMAGE_FOLDER_PATH, f) for f in os.listdir(IMAGE_FOLDER_PATH)]
     if len(image_paths) >0 :
         FACE_COUNT = len(image_paths)
         for image_path in image_paths:
@@ -106,7 +104,7 @@ def trainwith_existing_images():
             nbr = int(os.path.split(nbr)[1].split("/")[0])
             images.append(image_set)
             labels.append(nbr)
-        recognizer.train(images, np.array(labels))
+        RECOGNIZER.train(images, np.array(labels))
 trainwith_existing_images()
 start_webcam(mirror=True)
 
